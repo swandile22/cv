fda2aa0470a664087306696d1472efbfb8027177
swandile22 <sandile@spitfireinbound.com>
2024-03-07 12:35:18 +0200

    🚧 Added swiper functionality for Tech stack

---
+++ index.js
@@ -0,0 +1,19 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+    slidesPerView: 1.5,
+    spaceBetween: 1,
+    breakpoints: {
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        },
+    },
+});
